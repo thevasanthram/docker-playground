1ec82978d6d893337ae946e53a49e3701cc62f31
Vasanth Ram <82960398+thevasanthram@users.noreply.github.com>
2023-02-04 23:50:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 8080;
+const PORT = 8000;
 const HOST = "127.0.0.1";
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
